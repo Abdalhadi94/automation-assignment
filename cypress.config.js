79de7099d5c35b25a8ad4fe5a6a0b559a91c63ed
Abdelhadi Qudah <abdalhadi.qudah@gmail.com>
2025-07-30 00:47:45 +0300

    Update Cypress config timeouts

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+   defaultCommandTimeout: 10000, // default wait time for .get, .contains, etc.
+  pageLoadTimeout: 60000,       // increase if pages load slowly
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
